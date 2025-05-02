202657ae02437f3f81898b079b53c8c34ca227a7
TuNombre <TuCorreo@example.com>
2025-05-02 00:58:30 -0500

    Update main.py
    - Se cambia el carácter especial → por - para evitar problemas de compatibilidad con algunas consolas.
    - Se crea la función obtener_tiempo_ejecucion para que el usuario que va a ejecutar la simulación pueda establecer el tiempo por el cual va a realizarse el prceso.
    - Se crea la función generar_datos_aleatorios para obtener datos aleatorios en el tamaño de los paquetes.
    - Se establece un tiempo de espero aleatorio para que los paquetes se envien en distintos momentos de la ejecución.
    - Se agrega import sys sys.stdout.flush() para forzar que el texto pendiente en el búfer de salida estándar (stdout) se imprima inmediatamente en la consola y podamos ver la ejecución en tiempo real.

--- simulador/main.py
+++ simulador/main.py
@@ -1,7 +1,27 @@
+import time
+import random
+import string
 from core.nodo import Nodo
 from core.enlace import Enlace
 
+def obtener_tiempo_ejecucion():
+    while True:
+        try:
+            tiempo_ejecucion = input("Ingrese el tiempo de ejecución de la simulación en segundos: ").strip()
+            tiempo_ejecucion = float(tiempo_ejecucion)
+            if tiempo_ejecucion <= 0:
+                print("Por favor ingrese un número mayor que 0.")
+            else:
+                return tiempo_ejecucion
+        except ValueError:
+            print("Entrada no válida. Asegúrese de ingresar un número válido.")
+
+def generar_datos_aleatorios(tamano):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=tamano))
+
 if __name__ == '__main__':
+    tiempo_ejecucion = obtener_tiempo_ejecucion()
+
     n1 = Nodo("N1")
     n2 = Nodo("N2")
 
@@ -9,12 +29,25 @@ if __name__ == '__main__':
 
     # Simulación TCP: Handshake (simplificado)
     print("[TCP] Handshake iniciado")
-    print("N1 → SYN → N2")
-    print("N2 → SYN-ACK → N1")
-    print("N1 → ACK → N2")
+    print("N1 - SYN - N2")
+    print("N2 - SYN-ACK - N1")
+    print("N1 - ACK - N2")
 
-    # Envío de ráfaga de datos (1 MB)
-    datos = "1" * (1024 * 1024)  # 1 MB en caracteres
-    n1.enviar(enlace, datos)
+    max_paquetes = int(tiempo_ejecucion * random.uniform(1.5, 3))
+    num_paquetes = random.randint(int(tiempo_ejecucion), max_paquetes)
+
+    for i in range(num_paquetes):
+        tamano_aleatorio = random.randint(512 * 1024, 2 * 1024 * 1024)
+        datos = generar_datos_aleatorios(tamano_aleatorio)
+
+        tamanio_bits = len(datos) * 8
+        print(f"\n[Simulación] Enviando paquete {i+1}/{num_paquetes}")
+        print(f"[{n1.id}] Enviando datos: {tamanio_bits} bits")
+        n1.enviar(enlace, datos)
+        print(f"[Simulación] Paquete {i+1} enviado")
+        tiempo_espera = random.uniform(0.5, 2)
+        time.sleep(tiempo_espera)
+        import sys
+        sys.stdout.flush()
 
     print("\n[FIN] Simulación completada.")
